dd29597562701edca32ed87e1ca0504c4c265b7f
unknown <univupwork@gmail.com>
2023-02-08 10:24:15 -0800

    first

---
+++ gradebook/gradebook/scores/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .models import Subject
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('semesterone', views.semesterone, name='semesterone'),
+    path('semestertwo', views.semestertwo, name='semestertwo'),
+    path('semesterthree', views.semesterthree, name='semesterthree'),
+    path('semesterfive', views.semesterfive, name='semesterfive'),
+    path('semestersix', views.semestersix, name='semestersix'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+] 
